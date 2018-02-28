3d12a3a087f97a10c8ebd116d175764c4e9f5ada
tanim57 <tanim_57@live.com>
2018-02-28 18:41:28 +0600

    Add two class

--- app/src/main/java/com/tanim/year71/DividerItemDecoration.java
+++ app/src/main/java/com/tanim/year71/DividerItemDecoration.java
@@ -1,4 +1,4 @@
-package com.tanim.smsmania.ui;
+package com.tanim.year71;
 
 import android.content.Context;
 import android.graphics.Canvas;
@@ -6,8 +6,6 @@ import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-
-import com.tanim.smsmania.R;
 
 
 /**
